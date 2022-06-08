5fc0f57cc116881d9ebc4255222da06078d77d24
Stephen <stvlley.amzn@gmail.com>
2022-06-08 19:27:47 -0400

    fixxed console warnings > finish context submit

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -1,39 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Registration() {
 
 
+     const [email, setEmail] = useState("")
+     const [password, setPassword] = useState("")
 
-
-    const state = {
-        email: "",
-        password: "",
-        password_confirmation: "",
-        registrationErrors: ""
-
-
-    }
-
+    // const state = {
+    //     email: "",
+    //     password: "",
+    //     password_confirmation: "",
+    //     registrationErrors: ""
+    // }
+    console.log(email)
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log('form submitted')
+        console.log(email)
+        console.log(password)
     }
 
 
     const handleChange = (e) => {
         e.preventDefault()
-        console.log('handle change', e)
+        console.log(e.target.value)
     }
 
     return (
 
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="email" name="email" placeholder='Email' value={this.state.email} onChange={handleChange} />
+                <input type="email" name="email" placeholder='Email'   onChange={(event) => {
+                setEmail(event.target.value)
+              }} />
 
-                <input type="password" name="password" placeholder='Password' value={this.state.password} onChange={handleChange} />
+                <input type="password" name="password" placeholder='Password'      onChange={(event) => {
+                setPassword(event.target.value)
+              }} />
 
-                <input type="password" name="password_confirmation" placeholder='Password Confirmation' value={this.state.password} onChange={handleChange} />
+                <input type="password" name="password_confirmation" placeholder='Password Confirmation'  onChange={handleChange} />
+
+                <button type='submit' >Submit</button>
 
             </form>
         </div>
